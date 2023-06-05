1afe82855e9c8d914cbf935cc9c7814991951546
KevinPasteur <kevin.pasteur@icloud.com>
2023-06-05 15:47:03 +0200

    Remove footer

--- src/components/AppFooter.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <footer class="text-muted py-5 bg-dark">
-    <div class="container">
-      <p class="mb-1 text-white">All right reserved &copy; 2024 example.com</p>
-    </div>
-  </footer>
-</template>
