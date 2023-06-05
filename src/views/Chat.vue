20691a633df9eecfb08bff7ac899c093e90e859f
KevinPasteur <kevin.pasteur@icloud.com>
2023-06-05 15:47:55 +0200

    add views for Chat, Concours, Sondage

---
+++ src/views/Chat.vue
@@ -0,0 +1,31 @@
+<template>
+  <main class="container min-h-content mb-3 mt-1">
+    <div class="card chat-card">
+      <div class="row g-0">
+        <ChatBox @renderChat="renderChat" />
+      </div>
+    </div>
+  </main>
+</template>
+<script>
+import ChatBox from "../components/ChatBox.vue";
+
+export default {
+  components: { ChatBox },
+  data() {
+    return {
+      chatId: null,
+      startChat: false,
+    };
+  },
+  methods: {
+    /*
+     * To open a new chat
+     *we pass this method to ChatSidebar component
+     */
+    renderChat() {
+      this.startChat = true;
+    },
+  },
+};
+</script>
---
+++ src/views/Concours.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="container min-h-content py-5 text-center">
+    <div class="row py-lg-5">
+      <div class="col">
+        <h2>Page - Concours</h2>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {};
+</script>
