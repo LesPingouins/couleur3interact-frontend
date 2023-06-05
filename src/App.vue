bea82ea2b89ef2253e615c237eef2371a9f42af5
KevinPasteur <kevin.pasteur@icloud.com>
2023-06-05 11:11:22 +0200

    update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,30 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <router-view />
 </template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<script>
+import { computed, reactive } from "vue";
+import { useHead } from "@vueuse/head";
+export default {
+  setup() {
+    //creating dynamic meta tags
+    const siteData = reactive({
+      title: `My Chat App`,
+      description: `My beautiful Chat App`,
+    });
+    useHead({
+      // Can be static or computed
+      title: computed(() => siteData.title),
+      meta: [
+        {
+          name: `description`,
+          content: computed(() => siteData.description),
+        },
+      ],
+    });
+  },
+  async mounted() {
+    //loading the store data when the app start
+    await this.$store.commit("initialiseStore");
+  },
+};
+</script>
