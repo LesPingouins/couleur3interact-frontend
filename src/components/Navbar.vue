c1192babc0307509f47d013df8567c51e4c6fc0a
KevinPasteur <kevin.pasteur@icloud.com>
2023-06-05 15:49:59 +0200

    add navbar component

---
+++ src/components/Navbar.vue
@@ -0,0 +1,66 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-light rounded-pill">
+    <div class="navbar-nav flex-row justify-content-around w-100 shadow-inner">
+      <router-link
+        class="nav-item nav-link text-white"
+        :class="$route.name == 'Chat' ? 'active' : ''"
+        aria-current="page"
+        :to="{ name: 'Chat' }"
+        >Chat</router-link
+      >
+
+      <router-link
+        class="nav-item nav-link text-white"
+        :class="$route.name == 'Concours' ? 'active' : ''"
+        aria-current="page"
+        :to="{ name: 'Concours' }"
+        >Concours</router-link
+      >
+
+      <router-link
+        class="nav-item nav-link text-white"
+        :class="$route.name == 'Sondage' ? 'active' : ''"
+        aria-current="page"
+        :to="{ name: 'Sondage' }"
+        >Sondage</router-link
+      >
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "Navbar",
+  data() {
+    return {
+      notifications: [],
+    };
+  },
+  methods: {
+    logout() {
+      //this.$store.commit("Logout");
+      //this.$router.push("/login");
+    },
+  },
+};
+</script>
+
+<style scoped>
+.navbar {
+  background-image: linear-gradient(180deg, #ff003a, #f130e9);
+  /*background-image: linear-gradient(180deg, #ff003a, #f130e9);*/
+  box-shadow: 0px 5px 10px rgba(0, 0, 0, 0.25) inset;
+}
+
+.active {
+  border: none;
+
+  background-image: white;
+
+  padding-left: 1em;
+  padding-right: 1em;
+
+  border-radius: var(--bs-border-radius-pill) !important;
+  box-shadow: 0px 5px 10px rgba(0, 0, 0, 0.25);
+}
+</style>
