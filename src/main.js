d7bd2719dcb7c66c8205db856487fdc0983ca3df
KevinPasteur <kevin.pasteur@icloud.com>
2023-06-05 11:10:32 +0200

    import bootstrap, router, store

--- src/main.js
+++ src/main.js
@@ -1,5 +1,13 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from "vue";
+import "./style.css";
+import App from "./App.vue";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap";
+import "bootstrap-icons/font/bootstrap-icons.css";
+import router from "./router";
+import store from "./store";
+import { createHead } from "@vueuse/head";
 
-createApp(App).mount('#app')
+const head = createHead();
+
+createApp(App).use(head).use(router).use(store).mount("#app");
