bf661430633c1672ff952bee1f37d59cfc9a2e7f
KevinPasteur <kevin.pasteur@icloud.com>
2023-06-07 09:14:49 +0200

    remove col

--- src/components/concours/FormulaireBox.vue
+++ src/components/concours/FormulaireBox.vue
@@ -1,19 +1,15 @@
 <template>
-	<div class="col-12 col-lg-7 col-xl-9">
-		<div class="position-relative">
-			<div id="formulaireBox">
-				<ButtonRetour />
-				<TitreConcours texte="Vos données personelles" />
-				<InputConcours label="Nom" placeholder="Saisissez votre nom" />
-				<InputConcours label="Prénom" placeholder="Saisissez votre prénom" />
-				<InputConcours label="Adresse" placeholder="Saisissez votre adresse" />
-				<InputConcours label="NPA" placeholder="Saisissez votre NPA" />
-				<InputConcours label="Ville" placeholder="Saisissez votre ville" />
-				<InputConcours label="E-mail" placeholder="Saisissez votre e-mail" />
-				<ButtonConcours buttonText="Envoyer" />
-			</div>
-		</div>
-	</div>
+  <div id="formulaireBox">
+    <ButtonRetour />
+    <TitreConcours texte="Vos données personelles" />
+    <InputConcours label="Nom" placeholder="Saisissez votre nom" />
+    <InputConcours label="Prénom" placeholder="Saisissez votre prénom" />
+    <InputConcours label="Adresse" placeholder="Saisissez votre adresse" />
+    <InputConcours label="NPA" placeholder="Saisissez votre NPA" />
+    <InputConcours label="Ville" placeholder="Saisissez votre ville" />
+    <InputConcours label="E-mail" placeholder="Saisissez votre e-mail" />
+    <ButtonConcours buttonText="Envoyer" />
+  </div>
 </template>
 
 <script>
@@ -23,7 +19,7 @@ import TitreConcours from "./TitreConcours.vue";
 import ButtonRetour from "./ButtonRetour.vue";
 
 export default {
-	components: { ButtonConcours, InputConcours, TitreConcours, ButtonRetour },
+  components: { ButtonConcours, InputConcours, TitreConcours, ButtonRetour },
 };
 </script>
 
--- src/views/Concours.vue
+++ src/views/Concours.vue
@@ -1,18 +1,18 @@
 <template>
-	<div class="container min-h-content py-5 text-center">
-		<div class="row py-lg-5">
-			<div class="col">
-				<FormulaireBox />
-			</div>
-		</div>
-	</div>
+  <div class="container-fluid min-h-content py-5 text-center">
+    <div class="row">
+      <div class="col">
+        <FormulaireBox />
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 import FormulaireBox from "../components/concours/FormulaireBox.vue";
 
 export default {
-	components: { FormulaireBox },
+  components: { FormulaireBox },
 };
 </script>
 
