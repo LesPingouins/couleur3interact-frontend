b4d6a7fdb3a66e8aa46d8b79ac30d810c3e798c4
murier <cindy.murier@epfl.ch>
2023-06-07 08:54:11 +0200

    finish view Concours

---
+++ src/components/concours/ButtonConcours.vue
@@ -0,0 +1,19 @@
+<template>
+	<button class="concours-btn">
+		<span class="text-btn">{{ buttonText }}</span>
+	</button>
+</template>
+
+<script>
+export default {
+	name: "ButtonConcours",
+	props: {
+		buttonText: {
+			type: String,
+			required: true,
+		},
+	},
+};
+</script>
+
+<style scoped></style>
---
+++ src/components/concours/FormulaireBox.vue
@@ -0,0 +1,30 @@
+<template>
+	<div class="col-12 col-lg-7 col-xl-9">
+		<div class="position-relative">
+			<div id="formulaireBox">
+				<ButtonRetour />
+				<TitreConcours texte="Vos données personelles" />
+				<InputConcours label="Nom" placeholder="Saisissez votre nom" />
+				<InputConcours label="Prénom" placeholder="Saisissez votre prénom" />
+				<InputConcours label="Adresse" placeholder="Saisissez votre adresse" />
+				<InputConcours label="NPA" placeholder="Saisissez votre NPA" />
+				<InputConcours label="Ville" placeholder="Saisissez votre ville" />
+				<InputConcours label="E-mail" placeholder="Saisissez votre e-mail" />
+				<ButtonConcours buttonText="Envoyer" />
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+import ButtonConcours from "./ButtonConcours.vue";
+import InputConcours from "./InputConcours.vue";
+import TitreConcours from "./TitreConcours.vue";
+import ButtonRetour from "./ButtonRetour.vue";
+
+export default {
+	components: { ButtonConcours, InputConcours, TitreConcours, ButtonRetour },
+};
+</script>
+
+<style scoped></style>
