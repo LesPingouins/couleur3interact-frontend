7fd206d49433e698c818a2d5b1f9fc024d30657b
KevinPasteur <kevin.pasteur@icloud.com>
2023-06-07 16:31:02 +0200

    add link to ChoixConnexion

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,23 +1,23 @@
 <template>
   <div class="container-fluid d-flex justify-content-between">
-      <router-link
-        class=" nav-link"
-        :class="$route.name == 'Chat' ? 'active' : ''"
-        aria-current="page"
-        :to="{ name: 'Chat' }"
-        >
-        <div class="d-flex justify-content-between align-self-center">
-          <span class="material-symbols-outlined">
-            account_circle
-          </span>
-          <div class="d-flex connect align-self-center">
-          <b>Me connecter</b></div>
-        </div></router-link>
-     
-      <div class="nav-item nav-link d-flex align-self-center">
-        <ButtonDarkMode />
-      </div>
+    <router-link
+      class="nav-link"
+      :class="$route.name == 'ChoixConnexion' ? 'active' : ''"
+      aria-current="page"
+      :to="{ name: 'ChoixConnexion' }"
+    >
+      <div class="d-flex justify-content-between align-self-center">
+        <span class="material-symbols-outlined"> account_circle </span>
+        <div class="d-flex connect align-self-center">
+          <b>Me connecter</b>
+        </div>
+      </div></router-link
+    >
+
+    <div class="nav-item nav-link d-flex align-self-center">
+      <ButtonDarkMode />
     </div>
+  </div>
 </template>
 
 <script>
@@ -31,20 +31,19 @@ export default {
 </script>
 
 <style scoped>
-.container-fluid{
-  background-color: #FFFFFF;
+.container-fluid {
+  background-color: #ffffff;
   padding-left: 5%;
   padding-right: 5%;
   padding-top: 1%;
   padding-bottom: 1%;
-  
 }
 
-.material-symbols-outlined{
+.material-symbols-outlined {
   font-size: 40px;
   padding-right: 10px;
 }
-.connect{
+.connect {
   font-size: 20px;
 }
 </style>
