6e699ecc107f34284c5abafac78dbb5fccfc6436
murier <cindy.murier@epfl.ch>
2023-06-08 15:29:24 +0200

    finish popupchat view

---
+++ src/components/chat/Popup.vue
@@ -0,0 +1,65 @@
+<template>
+	<div class="popup">
+		<div class="header" @click="togglePopup">
+			<i :class="chevronClass"></i>
+			<div class="time">
+				<span class="material-symbols-outlined"> hourglass_empty </span>
+				{{ timeLeft }} secondes restantes
+			</div>
+			<TitleChat text="Quel est votre style de musique favori ?" />
+		</div>
+		<div class="content" :class="{ expanded: isExpanded }">
+			<TextChat text="SONDAGE" />
+			<ButtonPopup buttonText="Non merci" />
+			<ButtonPopup buttonText="Je participe" />
+		</div>
+	</div>
+</template>
+
+<script>
+import TitleChat from "./TitleChat.vue";
+import TextChat from "./TextChat.vue";
+import ButtonPopup from "./ButtonPopup.vue";
+
+export default {
+	name: "Popup",
+
+	components: { TitleChat, TextChat, ButtonPopup },
+
+	data() {
+		return {
+			isExpanded: false,
+		};
+	},
+	props: {
+		timeLeft: {
+			type: Number,
+			required: true,
+		},
+	},
+	computed: {
+		chevronClass() {
+			return this.isExpanded ? "fa fa-chevron-up" : "fa fa-chevron-down";
+		},
+	},
+	methods: {
+		togglePopup() {
+			this.isExpanded = !this.isExpanded;
+		},
+	},
+};
+</script>
+
+<style scoped>
+.content {
+	display: none;
+	transition: max-height 0.3s ease;
+	overflow: hidden;
+	max-height: 0;
+}
+
+.content.expanded {
+	display: block;
+	max-height: 500px; /* Adjust the max-height value as needed */
+}
+</style>
--- src/views/Chat.vue
+++ src/views/Chat.vue
@@ -1,57 +1,62 @@
 <template>
-  <main class="container-fluid">
-    <div class="card chat-card">
-      <div class="row g-0">
-        <ChatBox @renderChat="renderChat" />
-      </div>
-    </div>
-  </main>
+	<main class="container-fluid">
+		<div class="card chat-card">
+			<div class="row g-0">
+				<Popup timeLeft="25" />
+				<ChatBox @renderChat="renderChat" />
+			</div>
+		</div>
+	</main>
 </template>
 <script>
 import { deferredComputed } from "@vue/reactivity";
 import ChatBox from "../components/ChatBox.vue";
+import Popup from "../components/chat/Popup.vue";
 
 export default {
-  components: { ChatBox },
-  data() {
-    return {
-      chatId: null,
-      startChat: false,
-    }; 
-  },
-  methods: {
-    /*
-     * To open a new chat
-     *we pass this method to ChatSidebar component
-     */
-    renderChat() {
-      this.startChat = true;
-    },
-  },
+	components: { ChatBox, Popup },
+	data() {
+		return {
+			chatId: null,
+			startChat: false,
+		};
+	},
+	methods: {
+		/*
+		 * To open a new chat
+		 *we pass this method to ChatSidebar component
+		 */
+		renderChat() {
+			this.startChat = true;
+		},
+	},
 };
 </script>
 <style scoped>
-body{
- 
-  background: linear-gradient(180deg, rgba(255, 0, 58, 0.2), rgba(241, 48, 233, 0.2));
-
+body {
+	background: linear-gradient(
+		180deg,
+		rgba(255, 0, 58, 0.2),
+		rgba(241, 48, 233, 0.2)
+	);
 }
-main{
-background: linear-gradient(180deg, rgba(255, 0, 58, 0.2), rgba(241, 48, 233, 0.2));
-padding-left: 5%;
-padding-right: 5%;
-padding-bottom: 5%;
-
-
+main {
+	background: linear-gradient(
+		180deg,
+		rgba(255, 0, 58, 0.2),
+		rgba(241, 48, 233, 0.2)
+	);
+	padding-left: 5%;
+	padding-right: 5%;
+	padding-bottom: 5%;
 }
-.card{
-  background-color:#FFFFFF;
-  border-radius: 0px 0px 22px 22px;
-  border: none;
-  padding: 0px;
-
+.card {
+	background-color: #ffffff;
+	border-radius: 0px 0px 22px 22px;
+	border: none;
+	padding: 0px;
 }
-.container-fluid{
-  box-shadow: 0px 15px 8px rgba(0, 0, 0, 0.25) inset;
+.container-fluid {
+	box-shadow: 0px 15px 8px rgba(0, 0, 0, 0.25) inset;
 }
 </style>
