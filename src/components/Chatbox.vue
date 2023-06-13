fea3f7e893f4ae50ce18a7d1f53ef34988ac9717
Kévin <kevin.pasteur@icloud.com>
2023-06-13 08:53:10 +0200

    add call to API for Chat

--- src/components/Chatbox.vue
+++ src/components/Chatbox.vue
@@ -2,9 +2,9 @@
   <div class="col-12">
     <div class="position-relative">
       <div id="chatBox" class="chat-messages p-4">
-        <div v-for="message in messages" ref="messsageContainers" class="pb-4">
+        <div v-for="message in messages" ref="messageContainers" class="pb-4">
           <div class="flex-shrink-1 message-box rounded">
-            {{ message }}
+            <i> {{ getTime() }}</i> - salut : {{ message }}
           </div>
         </div>
       </div>
@@ -12,13 +12,8 @@
 
     <div class="flex-grow-0border-top">
       <div class="input-group">
-        <input
-          type="text"
-          v-model="message"
-          class="form-control"
-          placeholder="Écrire"
-        />
-       
+        <input type="text" @keyup.enter="onSubmit" v-model="message" class="form-control" placeholder="Écrire" />
+
       </div>
     </div>
   </div>
@@ -26,25 +21,31 @@
 
 <script>
 import axios from "axios";
+import moment from 'moment';
 
 export default {
   props: ["chat_id"],
-  setup() {},
-  created: function () {},
+  setup() { },
+  created: function () { },
   data() {
+    console.log();
     return {
       messages: [],
       chat: [],
       message: "",
+      username: "",
       isSendingForm: false,
       users: [],
     };
   },
   methods: {
+    getTime() {
+      return moment().format("HH:mm")
+    },
     // to auto-scroll to the new received  message
     scrollToLastMessage() {
       this.$nextTick(() => {
-        let items = this.$refs.messsageContainers;
+        let items = this.$refs.messageContainers;
         let last = items[items.length - 1];
         if (items.length > 0) {
           last.scrollIntoView({
@@ -62,7 +63,7 @@ export default {
       this.isSendingForm = true;
       axios
         .post(
-          this.$store.state.backendUrl + "/chat/send-text-message",
+          this.$store.state.backendUrl + "/chat",
           { message: this.message },
           {
             headers: {
@@ -72,15 +73,19 @@ export default {
         )
         .then((response) => {
           console.log(response);
-          this.isSendingForm = false;
-          this.message = "";
+          if (response.status === 200) {
+            this.isSendingForm = false;
+            this.message = "";
+
+          }
         })
         .catch((error) => {
           console.log(error);
-          /*  this.ShowError=true;
-            this.errorMgs = error.response.data.error;*/
+          this.ShowError = true;
+          this.errorMgs = error.response.data.error;
           this.isSendingForm = false;
         });
+
     },
 
     //to subscribe to the chat websocket channel
@@ -89,6 +94,11 @@ export default {
       Echo.channel("chat").listen("MessageSent", (e) => {
         this.messages.push(e.message);
         this.scrollToLastMessage();
+      });
+
+      //console.log(Echo.channel("poll").listen("PollSent"))
+      Echo.channel("poll").listen("PollSent", (e) => {
+        alert(e.question.question);
       });
     },
   },
@@ -101,43 +111,25 @@ export default {
   },
   mounted() {
     this.startWebSocket();
-    this.messages.push("test");
-    this.messages.push("test2");
-    this.messages.push("test2");  
-    this.messages.push("test2");
-    this.messages.push("test2");
-    this.messages.push("test2");
-    this.messages.push("test2");
-    this.messages.push("test2");
-    this.messages.push("test2");
-    this.messages.push("test2");
-    this.messages.push("test2");
-    this.messages.push("test2");
-    this.messages.push("test2");
-    this.messages.push("test2");
-    this.messages.push("test2");
-    this.messages.push("test2");
-   
   },
 };
 </script>
 <style scoped>
-
-.form-control{
+.form-control {
   background-color: none;
   border: 2px solid #1E1E1E;
   border-radius: 22px;
 
 }
-button{
+
+button {
   opacity: 0%;
 }
-.form-control:focus{
+
+.form-control:focus {
   border: 2px solid #1E1E1E;
   box-shadow: none;
 }
-.col-12{
 
-}
-
+.col-12 {}
 </style>
