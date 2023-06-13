beb8b3376962ebbe798353cf49d28ed66ad6af93
Kévin <kevin.pasteur@icloud.com>
2023-06-13 09:17:55 +0200

    merge conflicts

--- src/components/chat/login/ChoiceLoginBox.vue
+++ src/components/chat/login/ChoiceLoginBox.vue
@@ -1,41 +1,20 @@
 <template>
-<<<<<<< HEAD
-	<main class="container-fluid">
-		<div class="card chat-card">
-			<div class="choiceLoginBox">
-				<ButtonBackChat />
-				<div class="d-flex">
-					<TitleChat
-						text="Il faut vous connecter si vous voulez utiliser le chat !"
-						style="margin-bottom: 50px"
-					/>
-				</div>
-				<div class="d-flex justify-content-leftr align-self-end">
-					<TextChat text="Vous avez déjà un compte ?" />
-				</div>
-				<div class="d-flex justify-content-center">
-					<ButtonChat link="Connexion" buttonText="Se connecter" />
-				</div>
-				<div class="d-flex justify-content-center"><ButtonAnonymous /></div>
-=======
   <main class="container-fluid">
     <div class="card chat-card">
       <div class="choiceLoginBox">
         <ButtonBackChat />
         <div class="d-flex">
-          <TitleChat
-            text="Il faut vous connecter si vous voulez utiliser le chat !"
-            style="margin-bottom: 50px"
-          />
+          <TitleChat text="Il faut vous connecter si vous voulez utiliser le chat !" style="margin-bottom: 50px" />
         </div>
         <div class="d-flex justify-content-leftr align-self-end">
           <TextChat text="Vous avez déjà un compte ?" />
         </div>
         <div class="d-flex justify-content-center">
-          <ButtonChat buttonText="Se connecter" />
+          <ButtonChat link="Connexion" buttonText="Se connecter" />
         </div>
-        <div class="d-flex justify-content-center"><ButtonAnonymous /></div>
->>>>>>> af5282d02384d6b353d8c5b23db744f924126968
+        <div class="d-flex justify-content-center">
+          <ButtonAnonymous />
+        </div>
 
         <div class="d-flex justify-content-left">
           <TextChat text="Vous êtes un·e nouvel·le utilisateur·trice ?" />
@@ -71,12 +50,14 @@ export default {
 main {
   background: var(--red-gradient-op);
 }
+
 .card {
   background-color: var(--white);
   border-radius: 22px;
   border: none;
   padding: 5% 5% 10% 5%;
 }
+
 .container-fluid {
   box-shadow: 0px 15px 8px rgba(0, 0, 0, 0.25) inset;
 }
