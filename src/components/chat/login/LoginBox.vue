5bbd80e94520170f59dfbec9efe1124c5d259c59
Kévin <kevin.pasteur@icloud.com>
2023-06-13 08:55:38 +0200

    add refs

--- src/components/chat/login/LoginBox.vue
+++ src/components/chat/login/LoginBox.vue
@@ -4,24 +4,17 @@
 			<div id="loginBox">
 				<ButtonBackChat />
 				<TitleChat text="Se connecter" />
-				<InputChat label="E-mail *" placeholder="Saisissez votre e-mail" />
-				<InputChat
-					label="Mot de passe *"
-					placeholder="Saisissez votre mot de passe"
-				/>
-
+				<form id="form">
+					<InputChat ref="email" type="email" name="email" label="E-mail *"
+						placeholder="Saisissez votre e-mail" />
+					<InputChat ref="password" type="password" name="password" label="Mot de passe *"
+						placeholder="Saisissez votre mot de passe" />
+				</form>
 				<div class="space d-flex justify-content-between">
-					<CheckboxButton
-						title="Se souvenir de moi"
-						checkboxId="checkbox1"
-						checkboxValue="option1"
-					/>
-					<TextLinkChat
-						linkText="Mot de passe oublié ?"
-						link="https://www.example.com"
-					/>
+					<CheckboxButton title="Se souvenir de moi" checkboxId="checkbox1" checkboxValue="option1" />
+					<TextLinkChat linkText="Mot de passe oublié ?" link="https://www.example.com" />
 				</div>
-				<ButtonChat buttonText="Je me connecte!" />
+				<ButtonChat @click="submit" buttonText="Je me connecte!" />
 				<div class="d-flex justify-content-center align-self-end">
 					<TextChat class="textChat" text="Vous n'avez pas de compte ?" />
 				</div>
@@ -34,6 +27,7 @@
 </template>
 
 <script>
+import axios from "axios";
 import ButtonBackChat from "../ButtonBackChat.vue";
 import TitleChat from "../TitleChat.vue";
 import InputChat from "../InputChat.vue";
@@ -41,6 +35,7 @@ import CheckboxButton from "../CheckboxButton.vue";
 import TextLinkChat from "../TextLinkChat.vue";
 import ButtonChat from "../ButtonChat.vue";
 import TextChat from "../TextChat.vue";
+import router from "../../../router/index"
 
 export default {
 	components: {
@@ -52,28 +47,48 @@ export default {
 		ButtonChat,
 		TextChat,
 	},
+	methods: {
+		submit() {
+			const form = document.querySelector("#form");
+			const formData = new FormData(form);
+			console.log();
+			axios
+				.post(import.meta.env.VITE_BACKEND_URL + "/login",
+					{
+						email: this.$refs.email.value,
+						password: this.$refs.password.value
+					})
+				.then(function (response) {
+					if (response.status === 200) {
+						console.log(response);
+						router.push({ name: 'Chat' })
+					}
+				})
+
+		}
+	}
 };
 </script>
 
 <style scoped>
 main {
-	background: linear-gradient(
-		180deg,
-		rgba(255, 0, 58, 0.2),
-		rgba(241, 48, 233, 0.2)
-	);
+	background: linear-gradient(180deg,
+			rgba(255, 0, 58, 0.2),
+			rgba(241, 48, 233, 0.2));
 	padding-left: 5%;
 	padding-right: 5%;
 	padding-bottom: 5%;
 	padding-top: 5%;
 	height: 100%;
 }
+
 .card {
 	background-color: #ffffff;
 	border-radius: 22px;
 	border: none;
 	padding: 5% 5% 10% 5%;
 }
+
 .container-fluid {
 	box-shadow: 0px 15px 8px rgba(0, 0, 0, 0.25) inset;
 }
