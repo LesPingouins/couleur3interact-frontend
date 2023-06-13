e0c9ee2427a0a8d79c68884d65c50cea6773d511
Kévin <kevin.pasteur@icloud.com>
2023-06-13 14:33:39 +0200

    check state

--- src/components/Chatbox.vue
+++ src/components/Chatbox.vue
@@ -23,6 +23,7 @@
 <script>
 import axios from "axios";
 import moment from 'moment';
+import router from "../router";
 import store from "../store";
 
 export default {
@@ -62,33 +63,36 @@ export default {
 
     // to send new message
     async onSubmit() {
-      this.isSendingForm = true;
-      axios
-        .post(
-          this.$store.state.backendUrl + "/chat",
-          {
-            message: this.message,
-            username: this.username
-          },
-          {
-            headers: {
-              "Content-Type": "application/json",
+
+      if (this.$store.state.username !== "") {
+        this.isSendingForm = true;
+        axios
+          .post(
+            this.$store.state.backendUrl + "/chat",
+            {
+              message: this.message,
+              username: this.username
             },
-          }
-        )
-        .then((response) => {
-          console.log(response);
-          if (response.status === 200) {
+            {
+              headers: {
+                "Content-Type": "application/json",
+              },
+            }
+          )
+          .then((response) => {
+            console.log(response);
+            if (response.status === 200) {
+              this.isSendingForm = false;
+              this.message = "";
+            }
+          })
+          .catch((error) => {
+            console.log(error);
+            this.ShowError = true;
+            this.errorMgs = error.response.data.error;
             this.isSendingForm = false;
-            this.message = "";
-          }
-        })
-        .catch((error) => {
-          console.log(error);
-          this.ShowError = true;
-          this.errorMgs = error.response.data.error;
-          this.isSendingForm = false;
-        });
+          });
+      } else router.push("ChoixConnexion")
     },
 
     //to subscribe to the chat websocket channel
