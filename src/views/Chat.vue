c4639674dd096f5b0231ac9aa6acb720b3f2e057
Kévin <kevin.pasteur@icloud.com>
2023-06-13 08:57:25 +0200

    update css

--- src/views/Chat.vue
+++ src/views/Chat.vue
@@ -2,7 +2,7 @@
 	<main class="container-fluid">
 		<div class="card chat-card">
 			<div class="row g-0">
-				<Popup timeLeft="25" />
+				<Popup timeLeft=25 />
 				<ChatBox @renderChat="renderChat" />
 			</div>
 		</div>
@@ -34,28 +34,27 @@ export default {
 </script>
 <style scoped>
 body {
-	background: linear-gradient(
-		180deg,
-		rgba(255, 0, 58, 0.2),
-		rgba(241, 48, 233, 0.2)
-	);
+	background: linear-gradient(180deg,
+			rgba(255, 0, 58, 0.2),
+			rgba(241, 48, 233, 0.2));
 }
+
 main {
-	background: linear-gradient(
-		180deg,
-		rgba(255, 0, 58, 0.2),
-		rgba(241, 48, 233, 0.2)
-	);
+	background: linear-gradient(180deg,
+			rgba(255, 0, 58, 0.2),
+			rgba(241, 48, 233, 0.2));
 	padding-left: 5%;
 	padding-right: 5%;
 	padding-bottom: 5%;
 }
+
 .card {
 	background-color: #ffffff;
 	border-radius: 0px 0px 22px 22px;
 	border: none;
 	padding: 0px;
 }
+
 .container-fluid {
 	box-shadow: 0px 15px 8px rgba(0, 0, 0, 0.25) inset;
 }
